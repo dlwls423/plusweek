b5b71a09326bcdba420817c1c020eb029db8f94a
LeeYejin <leeyejin113@gmail.com>
2023-12-13 23:07:47 +0900

    feat: login API 쿠키 적용

--- src/main/java/com/sparta/plusweek/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/plusweek/domain/user/controller/UserController.java
@@ -6,11 +6,16 @@ import com.sparta.plusweek.domain.user.dto.UserSignupReq;
 import com.sparta.plusweek.domain.user.dto.UserSignupRes;
 import com.sparta.plusweek.domain.user.service.UserService;
 import com.sparta.plusweek.security.jwt.JwtUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,8 +38,32 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<UserLoginRes> login(@RequestBody UserLoginReq req, HttpServletResponse response){
         UserLoginRes res = userService.login(req);
-        response.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(req.getUsername()));
+        String token = jwtUtil.createToken(req.getUsername());
+        addCookie(token, response);
         return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    // 쿠키 읽어오기
+//    @GetMapping("/get-cookie")
+//    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
+//        System.out.println("value = " + value);
+//
+//        return "getCookie : " + value;
+//    }
+
+    public static void addCookie(String cookieValue, HttpServletResponse res) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
+
+            Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, cookieValue); // Name-Value
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 60);
+
+            // Response 객체에 Cookie 추가
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
 }
