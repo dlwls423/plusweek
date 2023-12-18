24cb0f33a27e3f3b3f14920a96fec808a922b5ad
leeyejin <leeyejin113@gmail.com>
2023-12-18 19:35:05 +0900

    feat: 게시글 단일 조회 API

--- src/main/java/com/sparta/plusweek/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/plusweek/domain/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.sparta.plusweek.domain.post.controller;
 import com.sparta.plusweek.domain.post.dto.PostCreatePostReq;
 import com.sparta.plusweek.domain.post.dto.PostCreatePostRes;
 import com.sparta.plusweek.domain.post.dto.PostGetAllPostsRes;
+import com.sparta.plusweek.domain.post.dto.PostGetPostRes;
 import com.sparta.plusweek.domain.post.service.PostReadService;
 import com.sparta.plusweek.domain.post.service.PostService;
 import com.sparta.plusweek.global.security.UserDetailsImpl;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,15 @@ public class PostController {
         Page<PostGetAllPostsRes> res = postReadService.getAllPosts(page - 1, size, sortBy, isAsc);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostGetPostRes> getPost(
+        @PathVariable(name = "postId") Long postId
+    ) {
+        PostGetPostRes res = postReadService.getPost(postId);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
 
     @PostMapping()
     public ResponseEntity<PostCreatePostRes> createPost(
--- src/main/java/com/sparta/plusweek/domain/post/service/PostServiceMapper.java
+++ src/main/java/com/sparta/plusweek/domain/post/service/PostServiceMapper.java
@@ -2,6 +2,7 @@ package com.sparta.plusweek.domain.post.service;
 
 import com.sparta.plusweek.domain.post.dto.PostCreatePostRes;
 import com.sparta.plusweek.domain.post.dto.PostGetAllPostsRes;
+import com.sparta.plusweek.domain.post.dto.PostGetPostRes;
 import com.sparta.plusweek.domain.post.entity.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -12,6 +13,8 @@ public interface PostServiceMapper {
     PostServiceMapper INSTANCE = Mappers.getMapper(PostServiceMapper.class);
 
     PostGetAllPostsRes toPostGetAllPostsRes(Post post);
+
+    PostGetPostRes toPostGetPostRes(Post post);
 
     PostCreatePostRes toPostCreatePostRes(Post post);
 }
